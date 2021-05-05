d580a6340ef4cac2852a170a1cf2541e6f24730c
ddnez <andre.bittar@gmail.com>
2021-05-05 18:51:55 +1200

    Adding ignore file. Allow loading prepared dataset from file.

--- utils.py
+++ utils.py
@@ -20,7 +20,8 @@ nlp = spacy.load('en_core_web_sm')
 
 class DatasetGenerator(Sequence):
     def __init__(self, x_set, y_set, batch_size=4):
-        self.x, self.y = x_set, y_set
+        self.x = x_set
+        self.y = LabelBinarizer().fit_transform(y_set)
 
         self.batch_size = batch_size
         self.indices = np.arange(len(self.x))
@@ -38,14 +39,7 @@ class DatasetGenerator(Sequence):
         np.random.shuffle(self.indices)
 
 
-def prepare_dataset(dataset_name='train'):
-    """
-    Save the data points into individual files.
-    :return:
-    """
-
-
-def load_data(dataset_name='train'):
+def load_data(dataset_name='train', reload=False):
     """
     Load data for the selected dataset.
 
@@ -60,20 +54,23 @@ def load_data(dataset_name='train'):
     df_labels = pd.DataFrame()
     df_users = pd.DataFrame()
 
-    if dataset_name == 'train':
-        df_users = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/train/task_A_train.posts.csv')
-        df_posts = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/train/shared_task_posts.csv')
-        df_labels = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/train/crowd_train.csv')
-    if dataset_name == 'test':
-        df_users = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/test/task_A_test.posts.csv')
-        df_posts = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/test/shared_task_posts_test.csv')
-        df_labels = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/test/crowd_test_A.csv')
+    if reload:
+        if dataset_name == 'train':
+            df_users = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/train/task_A_train.posts.csv')
+            df_posts = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/train/shared_task_posts.csv')
+            df_labels = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/train/crowd_train.csv')
+        if dataset_name == 'test':
+            df_users = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/test/task_A_test.posts.csv')
+            df_posts = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/test/shared_task_posts_test.csv')
+            df_labels = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/test/crowd_test_A.csv')
 
-    df_posts = df_posts.loc[df_posts.user_id.isin(df_users.user_id)]
-    df = df_users.merge(df_posts, on=['post_id', 'user_id', 'subreddit'])
-    df = df.merge(df_labels, on='user_id').sort_values(by=['user_id', 'timestamp'], ascending=[True, True])
-    df.post_title.fillna(value='', inplace=True)
-    df.post_body.fillna(value='', inplace=True)
+        df_posts = df_posts.loc[df_posts.user_id.isin(df_users.user_id)]
+        df = df_users.merge(df_posts, on=['post_id', 'user_id', 'subreddit'])
+        df = df.merge(df_labels, on='user_id').sort_values(by=['user_id', 'timestamp'], ascending=[True, True])
+        df.post_title.fillna(value='', inplace=True)
+        df.post_body.fillna(value='', inplace=True)
+    else:
+        df = pd.read_csv('umd_reddit_suicidewatch_dataset_v2/crowd/' + dataset_name + '/task_A_' + dataset_name + '.csv')
 
     return df
 
