72b354c8c08a98d94d65c77cd05e9643bfbadda0
ddnez <andre.bittar@gmail.com>
2021-05-05 23:29:11 +1200

    Added main

--- svm_baseline.py
+++ svm_baseline.py
@@ -8,29 +8,30 @@ from utils import load_data, spacy_tokenize
 
 SEED = 7
 
-df_train = load_data(dataset_name='train')
-df_train = df_train[['user_id', 'post_title', 'post_body', 'label']]
+if __name__ == '__main__':
+    df_train = load_data(dataset_name='train')
+    df_train = df_train[['user_id', 'post_title', 'post_body', 'label']]
 
-pipeline = Pipeline([
-    ('vectorizer', TfidfVectorizer(analyzer=spacy_tokenize)),
-    ('svm', LinearSVC(class_weight='balanced'))
-])
+    pipeline = Pipeline([
+        ('vectorizer', TfidfVectorizer(analyzer=spacy_tokenize)),
+        ('svm', LinearSVC(class_weight='balanced'))
+    ])
 
-# prepare the data
-X = [title + body for (title, body) in zip(df_train.post_title.tolist(), df_train.post_body.tolist())]
-y = df_train.label.to_list()
+    # prepare the data
+    X = [title + body for (title, body) in zip(df_train.post_title.tolist(), df_train.post_body.tolist())]
+    y = df_train.label.to_list()
 
-le = LabelEncoder()
-y_ = le.fit_transform(y)
+    le = LabelEncoder()
+    y_ = le.fit_transform(y)
 
-X_train, X_dev, y_train, y_dev = train_test_split(X, y_, test_size=0.2, random_state=SEED)
+    X_train, X_dev, y_train, y_dev = train_test_split(X, y_, test_size=0.2, random_state=SEED)
 
-# default configuration with spaCy tokenizer (lowercased words) gives best macro-averaged results
-# params = {'svm__C': [0.001, 0.01, 1.0, 10, 100], 'svm__class_weight': ['balanced', None]}
-# gs = GridSearchCV(pipeline, param_grid=params, cv=10, scoring='accuracy')
-# gs.fit(Xb_train, yb_train)
+    # default configuration with spaCy tokenizer (lowercased words) gives best macro-averaged results
+    # params = {'svm__C': [0.001, 0.01, 1.0, 10, 100], 'svm__class_weight': ['balanced', None]}
+    # gs = GridSearchCV(pipeline, param_grid=params, cv=10, scoring='accuracy')
+    # gs.fit(Xb_train, yb_train)
 
-pipeline.fit(X_train, y_train)
+    pipeline.fit(X_train, y_train)
 
-y_pred = pipeline.predict(X_dev)
-print(classification_report(y_dev, y_pred, target_names=le.classes_))
+    y_pred = pipeline.predict(X_dev)
+    print(classification_report(y_dev, y_pred, target_names=le.classes_))
