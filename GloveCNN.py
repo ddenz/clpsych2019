19609dc98486e6e089b9c9e7568ce4cde6c389ce
ddnez <andre.bittar@gmail.com>
2021-05-28 11:02:49 +1200

    Removed length argument in constructors - use embedding matrix

--- GloveCNN.py
+++ GloveCNN.py
@@ -7,14 +7,13 @@ from simple_elmo import ElmoModel
 
 
 class GloveCNN(Sequential):
-    def __init__(self, embedding_matrix, emb_len):
+    def __init__(self, embedding_matrix):
         super().__init__()
         self.emb_matrix = embedding_matrix
-        self.emb_len = emb_len
 
     def build_model_(self, lr=0.001, optimizer='adam', loss='categorical_crossentropy'):
-        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_len, input_length=MAX_LENGTH,
-                           weights=[self.emb_matrix], trainable=False))
+        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_matrix[0].shape[0],
+                           input_length=MAX_LENGTH, weights=[self.emb_matrix], trainable=False))
         self.add(Conv1D(64, 7, activation='relu', padding='same'))
         self.add(MaxPooling1D())
         self.add(Conv1D(128, 5, activation='relu', padding='same'))
@@ -29,8 +28,8 @@ class GloveCNN(Sequential):
         self.summary()
 
     def build_model(self, optimizer=Adam(lr=0.001), loss='categorical_crossentropy'):
-        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_len, input_length=MAX_LENGTH,
-                           weights=[self.emb_matrix], trainable=False))
+        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_matrix[0].shape[0],
+                           input_length=MAX_LENGTH, weights=[self.emb_matrix], trainable=False))
         self.add(Conv1D(2, 300, activation='relu', padding='same'))
         self.add(MaxPooling1D())
         self.add(Flatten())
@@ -41,14 +40,13 @@ class GloveCNN(Sequential):
 
 
 class GloveBiRNN(Sequential):
-    def __init__(self, embedding_matrix, emb_len):
+    def __init__(self, embedding_matrix):
         super().__init__()
         self.emb_matrix = embedding_matrix
-        self.emb_len = emb_len
 
     def build_model(self, optimizer=Adam(lr=0.001), loss='categorical_crossentropy'):
-        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_len, input_length=MAX_LENGTH,
-                           weights=[self.emb_matrix], trainable=False))
+        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_matrix[0].shape[0],
+                           input_length=MAX_LENGTH, weights=[self.emb_matrix], trainable=False))
         self.add(Bidirectional(SimpleRNN(64)))
         self.add(Dropout(0.5))
         self.add(Bidirectional(SimpleRNN(64)))
@@ -59,14 +57,12 @@ class GloveBiRNN(Sequential):
 
 
 if __name__ == '__main__':
-    #X_train, y_train, X_test, y_test, emb_matrix = prepare_sequential(merge=False, emb_name='glove-wiki-gigaword-300')
+    X_train, y_train, X_test, y_test, emb_matrix = prepare_sequential(merge=False, emb_name='glove-wiki-gigaword-300')
 
-    #glove_cnn = GloveCNN(emb_matrix, emb_matrix[0].shape[0])
+    #glove_cnn = GloveCNN(emb_matrix)
     #glove_cnn.build_model()
     #history = glove_cnn.fit(X_train, y_train, batch_size=32, epochs=20)
 
-    X_train, y_train, X_test, y_test, emb_matrix = prepare_sequential(merge=True)
-
-    glove_rnn = GloveBiRNN(emb_matrix, emb_matrix[0].shape[0])
+    glove_rnn = GloveBiRNN(emb_matrix)
     glove_rnn.build_model()
     history = glove_rnn.fit(X_train, y_train, batch_size=32, epochs=20)
