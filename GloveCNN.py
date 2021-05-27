4750497b5218f259e82030042f9c858699a0e18e
ddnez <andre.bittar@gmail.com>
2021-05-28 11:55:19 +1200

    Test GloveGRU

--- GloveCNN.py
+++ GloveCNN.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Conv1D, Dense, Dropout, Embedding, Flatten, MaxPooling1D, SimpleRNN, Bidirectional
+from keras.layers import Conv1D, Dense, Dropout, Embedding, Flatten, MaxPooling1D, SimpleRNN, Bidirectional, GRU
 from keras.optimizers import Adam
 from keras.wrappers.scikit_learn import KerasClassifier
 from utils import prepare_sequential, MAX_LENGTH
@@ -56,6 +56,23 @@ class GloveBiRNN(Sequential):
         self.summary()
 
 
+class GloveGRU(Sequential):
+    def __init__(self, embedding_matrix):
+        super().__init__()
+        self.emb_matrix = embedding_matrix
+
+    def build_model(self, optimizer=Adam(lr=0.001), loss='categorical_crossentropy'):
+        self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_matrix[0].shape[0],
+                           input_length=MAX_LENGTH, weights=[self.emb_matrix], trainable=False))
+        self.add(GRU(64))
+        self.add(Dropout(0.5))
+        self.add(GRU(64))
+        self.add(Dropout(0.5))
+        self.add(Dense(4, activation='softmax'))
+        self.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+        self.summary()
+
+
 if __name__ == '__main__':
     X_train, y_train, X_test, y_test, emb_matrix = prepare_sequential(merge=False, emb_name='glove-wiki-gigaword-300')
 
@@ -63,6 +80,10 @@ if __name__ == '__main__':
     #glove_cnn.build_model()
     #history = glove_cnn.fit(X_train, y_train, batch_size=32, epochs=20)
 
-    glove_rnn = GloveBiRNN(emb_matrix)
-    glove_rnn.build_model()
-    history = glove_rnn.fit(X_train, y_train, batch_size=32, epochs=20)
+    #glove_rnn = GloveBiRNN(emb_matrix)
+    #glove_rnn.build_model()
+    #history = glove_rnn.fit(X_train, y_train, batch_size=32, epochs=20)
+
+    glove_gru = GloveGRU(emb_matrix)
+    glove_gru.build_model()
+    history = glove_gru.fit(X_train, y_train, batch_size=32, epochs=20)
