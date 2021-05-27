ce5a586008894dc49415977dd433b0d4e016fe79
ddnez <andre.bittar@gmail.com>
2021-05-28 11:36:05 +1200

    Test GloveBiRNN

--- GloveCNN.py
+++ GloveCNN.py
@@ -47,7 +47,7 @@ class GloveBiRNN(Sequential):
     def build_model(self, optimizer=Adam(lr=0.001), loss='categorical_crossentropy'):
         self.add(Embedding(input_dim=self.emb_matrix.shape[0], output_dim=self.emb_matrix[0].shape[0],
                            input_length=MAX_LENGTH, weights=[self.emb_matrix], trainable=False))
-        self.add(Bidirectional(SimpleRNN(64, return_sequences=True, return_state=True)))
+        self.add(Bidirectional(SimpleRNN(64, return_sequences=True)))
         self.add(Dropout(0.5))
         self.add(Bidirectional(SimpleRNN(64)))
         self.add(Dropout(0.5))
