70cfa2d248b07d5e9ce123b36cc043f959fde869
ddnez <andre.bittar@gmail.com>
2021-06-01 21:29:44 +1200

    Testing functional implementation - incomplete

--- FusionLayer.py
+++ FusionLayer.py
@@ -1,5 +1,5 @@
 from keras.layers import Conv1D, Dense, Dropout, Embedding, Flatten, MaxPooling1D, AveragePooling1D, SimpleRNN, \
-    Bidirectional, GRU, LSTM, Concatenate
+    Bidirectional, GRU, LSTM, Concatenate, Average
 from keras.models import Sequential
 from keras.layers import Input
 from tensorflow.keras.layers import Attention
@@ -22,9 +22,9 @@ if __name__ == '__main__':
 
     # GloveCNN
     gcnn_c1 = Conv1D(2, 300, activation='relu', padding='same')(e)
-    gcnn_mp1 = MaxPooling1D()(gcnn_c1)
+    # gcnn_mp1 = MaxPooling1D()(gcnn_c1)
     # gcnn_f1 = Flatten()(gcnn_mp1)
-    gcnn_do1 = Dropout(0.5)(gcnn_mp1)
+    gcnn_do1 = Dropout(0.5)(gcnn_c1)
     # gcnn_d1 = Dense(4, activation='softmax')(gcnn_do1)
     gcnn_mp2 = MaxPooling1D()(gcnn_do1)
     gcnn_ap = AveragePooling1D()(gcnn_do1)
@@ -32,37 +32,38 @@ if __name__ == '__main__':
     gcnn_out = Concatenate()([gcnn_mp2, gcnn_ap])
 
     # GloveBiRNN
-    grnn_r1 = Bidirectional(SimpleRNN(64, return_sequences=True))(e)
+    grnn_r1 = Bidirectional(SimpleRNN(64, return_sequences=False))(e)
     grnn_do1 = Dropout(0.5)(grnn_r1)
-    grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=True))(grnn_do1)
+    # grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=True))(grnn_do1)
+    grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=False))(e)
     grnn_do2 = Dropout(0.5)(grnn_r2)
     # grnn_d1 = Dense(4, activation='softmax')(grnn_do2)
     grnn_mp = MaxPooling1D()(grnn_do2)
-    grnn_ap = AveragePooling1D()(grnn_do2)
+    grnn_ap = Average()([grnn_do1, grnn_do2])
     # grnn_att = Attention(grnn_do2)
     grnn_out = Concatenate()([grnn_mp, grnn_ap])
 
     # GloveGRU
-    ggru_r1 = GRU(64, return_sequences=True)(e)
+    ggru_r1 = GRU(64, return_sequences=False)(e)
     ggru_do1 = Dropout(0.5)(ggru_r1)
-    ggru_r2 = GRU(64, return_sequences=False)(ggru_do1)
+    ggru_r2 = GRU(64, return_sequences=False)(e)
     ggru_do2 = Dropout(0.5)(ggru_r2)
     # ggru_d1 = Dense(4, activation='softmax')(ggru_do2)
     ggru_mp = MaxPooling1D()(ggru_do2)
-    ggru_ap = AveragePooling1D()(ggru_do2)
+    ggru_ap = Average()([ggru_do1, grnn_do2])
     # ggru_att = Attention(ggru_do2)
     ggru_out = Concatenate()([ggru_mp, ggru_ap])
 
     # GloveBiLSTM
     glstm_r1 = Bidirectional(LSTM(32, activation='sigmoid', recurrent_dropout=0.2, recurrent_activation='sigmoid',
-                                  return_sequences=True))(e)
+                                  return_sequences=False))(e)
     glstm_do1 = Dropout(0.5)(glstm_r1)
     glstm_r2 = Bidirectional(LSTM(32, activation='sigmoid', recurrent_dropout=0.2, recurrent_activation='sigmoid',
-                                  return_sequences=False))(glstm_do1)
+                                  return_sequences=False))(e)
     glstm_do2 = Dropout(0.5)(glstm_r2)
     # glstm_d1 = Dense(4, activation='softmax')(glstm_do2)
     glstm_mp = MaxPooling1D()(glstm_do2)
-    glstm_ap = AveragePooling1D()(glstm_do2)
+    glstm_ap = Average()([glstm_do1, glstm_do2])
     # glstm_att = Attention(glstm_do2)
     glstm_out = Concatenate()([grnn_mp, grnn_ap])
 
