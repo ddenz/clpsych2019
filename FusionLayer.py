0b92bd515502a5ffd015e6587cd9e64b472c8778
ddnez <andre.bittar@gmail.com>
2021-06-02 23:13:41 +1200

    Testing functional implementation - incomplete

--- FusionLayer.py
+++ FusionLayer.py
@@ -1,7 +1,7 @@
 import keras
 import numpy as np
 from keras.layers import Conv1D, Dense, Dropout, Embedding, MaxPooling1D, AveragePooling1D, SimpleRNN, Input, \
-    Bidirectional, GRU, LSTM, Concatenate
+    Bidirectional, GRU, LSTM, concatenate
 from keras.losses import CategoricalCrossentropy
 from keras.optimizers import Adam
 from utils import prepare_sequential, MAX_LENGTH
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     gcnn_do1 = Dropout(0.5)(gcnn_c1)
     gcnn_mp2 = MaxPooling1D()(gcnn_do1)
     gcnn_ap = AveragePooling1D()(gcnn_do1)
-    gcnn_out = Concatenate()([gcnn_mp2, gcnn_ap])
+    gcnn_out = concatenate([gcnn_mp2, gcnn_ap])
 
     # GloveBiRNN
     grnn_r1 = Bidirectional(SimpleRNN(64, return_sequences=True))(e)
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     grnn_do2 = Dropout(0.5)(grnn_r2)
     grnn_mp = MaxPooling1D()(grnn_do2)
     grnn_ap = AveragePooling1D()(grnn_do2)
-    grnn_out = Concatenate()([grnn_mp, grnn_ap])
+    grnn_out = concatenate([grnn_mp, grnn_ap])
 
     # GloveGRU
     ggru_r1 = GRU(64, return_sequences=True)(e)
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     ggru_do2 = Dropout(0.5)(ggru_r2)
     ggru_mp = MaxPooling1D()(ggru_do2)
     ggru_ap = AveragePooling1D()(ggru_do2)
-    ggru_out = Concatenate()([ggru_mp, ggru_ap])
+    ggru_out = concatenate([ggru_mp, ggru_ap])
 
     # GloveBiLSTM
     glstm_r1 = Bidirectional(LSTM(32, activation='sigmoid', recurrent_dropout=0.2, recurrent_activation='sigmoid',
@@ -49,9 +49,9 @@ if __name__ == '__main__':
     glstm_do2 = Dropout(0.5)(glstm_r2)
     glstm_mp = MaxPooling1D()(glstm_do2)
     glstm_ap = AveragePooling1D()(glstm_do2)
-    glstm_out = Concatenate()([glstm_mp, glstm_ap])
+    glstm_out = concatenate([glstm_mp, glstm_ap])
 
-    output_1 = Concatenate([gcnn_out, grnn_out, ggru_out, glstm_out])
+    output_1 = concatenate([gcnn_out, grnn_out, ggru_out, glstm_out])
     outputs = Dense(4)(output_1)
 
     model = keras.Model(inputs=inputs, outputs=outputs, name='fusion_model')
