a82f20bc460d68256780068ee65707f74a6df7cb
ddnez <andre.bittar@gmail.com>
2021-06-02 19:29:19 +1200

    Testing functional implementation - incomplete

--- FusionLayer.py
+++ FusionLayer.py
@@ -32,39 +32,39 @@ if __name__ == '__main__':
     gcnn_out = Concatenate()([gcnn_mp2, gcnn_ap])
 
     # GloveBiRNN
-    grnn_r1 = Bidirectional(SimpleRNN(64, return_sequences=False))(e)
+    grnn_r1 = Bidirectional(SimpleRNN(64, return_sequences=True))(e)
     grnn_do1 = Dropout(0.5)(grnn_r1)
-    # grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=True))(grnn_do1)
-    grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=False))(e)
+    grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=False))(grnn_do1)
+    # grnn_r2 = Bidirectional(SimpleRNN(64, return_sequences=False))(e)
     grnn_do2 = Dropout(0.5)(grnn_r2)
     # grnn_d1 = Dense(4, activation='softmax')(grnn_do2)
     grnn_mp = MaxPooling1D()(grnn_do2)
-    grnn_ap = Average()([grnn_do1, grnn_do2])
+    grnn_ap = AveragePooling1D()(grnn_do2)
     # grnn_att = Attention(grnn_do2)
     grnn_out = Concatenate()([grnn_mp, grnn_ap])
 
     # GloveGRU
-    ggru_r1 = GRU(64, return_sequences=False)(e)
+    ggru_r1 = GRU(64, return_sequences=True)(e)
     ggru_do1 = Dropout(0.5)(ggru_r1)
-    ggru_r2 = GRU(64, return_sequences=False)(e)
+    ggru_r2 = GRU(64, return_sequences=False)(ggru_do1)
     ggru_do2 = Dropout(0.5)(ggru_r2)
     # ggru_d1 = Dense(4, activation='softmax')(ggru_do2)
     ggru_mp = MaxPooling1D()(ggru_do2)
-    ggru_ap = Average()([ggru_do1, grnn_do2])
+    ggru_ap = AveragePooling1D()(ggru_do2)
     # ggru_att = Attention(ggru_do2)
     ggru_out = Concatenate()([ggru_mp, ggru_ap])
 
     # GloveBiLSTM
     glstm_r1 = Bidirectional(LSTM(32, activation='sigmoid', recurrent_dropout=0.2, recurrent_activation='sigmoid',
-                                  return_sequences=False))(e)
+                                  return_sequences=True))(e)
     glstm_do1 = Dropout(0.5)(glstm_r1)
     glstm_r2 = Bidirectional(LSTM(32, activation='sigmoid', recurrent_dropout=0.2, recurrent_activation='sigmoid',
-                                  return_sequences=False))(e)
+                                  return_sequences=False))(glstm_do1)
     glstm_do2 = Dropout(0.5)(glstm_r2)
     # glstm_d1 = Dense(4, activation='softmax')(glstm_do2)
     glstm_mp = MaxPooling1D()(glstm_do2)
-    glstm_ap = Average()([glstm_do1, glstm_do2])
+    glstm_ap = AveragePooling1D()(glstm_do2)
     # glstm_att = Attention(glstm_do2)
-    glstm_out = Concatenate()([grnn_mp, grnn_ap])
+    glstm_out = Concatenate()([glstm_mp, glstm_ap])
 
-    x = Concatenate([])
+    output_1 = Concatenate([gcnn_out, grnn_out, ggru_out, glstm_out])
